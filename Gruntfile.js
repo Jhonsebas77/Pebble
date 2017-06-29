77b829f72ed9165adc563510f94f945cd01ce76b
paula <paula@192.168.10.112>
2017-06-29 10:50:19 -0500

    v.0.1 Jun 29 10:50 Prueba

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+
+/* 
+ * Desarrollo Protegido por Derechos de Autor a nombre de Fruticola de Colombia S.A.S. 
+ * Proyecto: Liquidacion de Importaciones.
+ * Empresa: Fruticola de Colombia S.A.S.
+ * Responsable Proyecto: Sebastian Otalora 
+ * Responsable Contacto: sistemas@fruticoladecolombia.com | 3217209516
+ */
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+    });
+};
---
+++ public_html/HelloWorld.js
@@ -0,0 +1,11 @@
+
+/* 
+ * Desarrollo Protegido por Derechos de Autor a nombre de Fruticola de Colombia S.A.S. 
+ * Proyecto: Liquidacion de Importaciones.
+ * Empresa: Fruticola de Colombia S.A.S.
+ * Responsable Proyecto: Sebastian Otalora 
+ * Responsable Contacto: sistemas@fruticoladecolombia.com | 3217209516
+ */
+
+
+simply.title('Hello World!');
